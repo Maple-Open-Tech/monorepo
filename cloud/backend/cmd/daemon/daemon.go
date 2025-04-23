70469345dbed93d390cd3050d3481a54e411c98e
Bartlomiej Mika <bart@mikasoftware.com>
2025-04-23 00:47:20 -0400

    Initial commit for adding support for https://github.com/uber-go

--- cloud/backend/cmd/daemon/daemon.go
+++ cloud/backend/cmd/daemon/daemon.go
@@ -1,13 +1,13 @@
+// github.com/Maple-Open-Tech/monorepo/cloud/backend/cmd/daemon/daemon.go
 package daemon
 
 import (
-	// "context"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"net/http"
 
 	"github.com/spf13/cobra"
+	"go.uber.org/fx"
+
+	"github.com/Maple-Open-Tech/monorepo/cloud/backend/unifiedhttp"
 )
 
 func DaemonCmd() *cobra.Command {
@@ -15,7 +15,6 @@ func DaemonCmd() *cobra.Command {
 		Use:   "daemon",
 		Short: "Run the cloud-services backend",
 		Run: func(cmd *cobra.Command, args []string) {
-			log.Println("Running daemon......")
 			doRunDaemon()
 		},
 	}
@@ -23,40 +22,8 @@ func DaemonCmd() *cobra.Command {
 }
 
 func doRunDaemon() {
-	//
-	// STEP 1
-	// Load up our dependencies and configuration
-	//
-
-	// Load up our operating system interaction handlers, more specifically
-	// signals. The OS sends our application various signals based on the
-	// OS's state, we want to listen into the termination signals.
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGUSR1)
-
-	//
-	// STEP 2
-	// Load up our infrastructure and other dependencies
-	//
-
-	// Common
-
-	//
-	// STEP 3
-	// Load up our modules.
-	//
-
-	//
-	// STEP 4:
-	// Initialize our unified http server and task manager
-	//
-
-	//
-	// STEP 5:
-	// Unified execute of all the modules.
-	//
-
-	// Run in background
-
-	<-done
+	fx.New(
+		fx.Provide(unifiedhttp.NewGetHealthCheckHTTPHandler),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
 }
--- cloud/backend/go.mod
+++ cloud/backend/go.mod
@@ -7,4 +7,9 @@ require github.com/spf13/cobra v1.9.1
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/fx v1.23.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 )

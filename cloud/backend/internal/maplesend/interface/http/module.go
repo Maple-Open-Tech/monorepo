8ff0975f28fc891f26e5cb31f076c0fffcc6f776
Bartlomiej Mika <bart@mikasoftware.com>
2025-04-24 22:09:44 -0400

    More architecture simplification.

--- cloud/backend/internal/maplesend/interface/http/module.go
+++ cloud/backend/internal/maplesend/interface/http/module.go
@@ -3,6 +3,9 @@ package http
 import (
 	"go.uber.org/fx"
 
+	unifiedhttp "github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/manifold/interface/http"
+	commonhttp "github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/interface/http/common"
+	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/interface/http/gateway"
 	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/interface/http/middleware"
 )
 
@@ -10,6 +13,10 @@ func Module() fx.Option {
 	return fx.Options(
 		fx.Provide(
 			middleware.NewMiddleware,
+		),
+		fx.Provide(
+			unifiedhttp.AsRoute(commonhttp.NewGetMapleSendVersionHTTPHandler),
+			unifiedhttp.AsRoute(gateway.NewGatewayUserRegisterHTTPHandler),
 		),
 	)
 }
--- cloud/backend/internal/maplesend/module.go
+++ cloud/backend/internal/maplesend/module.go
@@ -3,10 +3,7 @@ package maplesend
 import (
 	"go.uber.org/fx"
 
-	unifiedhttp "github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/manifold/interface/http"
-	commonhttp "github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/interface/http/common"
-	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/interface/http/gateway"
-	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/interface/http/middleware"
+	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/interface/http"
 	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/repo"
 	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/service"
 	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/maplesend/usecase"
@@ -17,12 +14,6 @@ func Module() fx.Option {
 		repo.Module(),
 		usecase.Module(),
 		service.Module(),
-		fx.Provide(
-			middleware.NewMiddleware,
-		),
-		fx.Provide(
-			unifiedhttp.AsRoute(commonhttp.NewGetMapleSendVersionHTTPHandler),
-			unifiedhttp.AsRoute(gateway.NewGatewayUserRegisterHTTPHandler),
-		),
+		http.Module(),
 	)
 }

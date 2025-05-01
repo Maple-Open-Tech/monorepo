60ae038c81531feb2227f4af1e49b885866a7018
Bartlomiej Mika <bart@mikasoftware.com>
2025-05-01 01:08:58 -0400

    feat(mapleauth): Provide token verification and session services

--- cloud/backend/internal/mapleauth/service/module.go
+++ cloud/backend/internal/mapleauth/service/module.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/mapleauth/service/gateway"
 	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/mapleauth/service/me"
+	"github.com/Maple-Open-Tech/monorepo/cloud/backend/internal/mapleauth/service/token"
 )
 
 func Module() fx.Option {
 	return fx.Options(
 		fx.Provide(
+			token.NewTokenVerifyService,
+			token.NewTokenGetSessionService,
 			gateway.NewGatewayUserRegisterService,
 			gateway.NewGatewayLoginService,
 			gateway.NewGatewayLogoutService,

de29b4cd5774ee78f48954a7c1463fa8f2eb74ad
Bartlomiej Mika <bart@mikasoftware.com>
2025-05-09 21:57:13 -0400

    Implemented new structure of file.

--- cloud/backend/internal/papercloud/repo/file/storage/impl.go
+++ cloud/backend/internal/papercloud/repo/file/storage/impl.go
@@ -2,9 +2,6 @@
 package storage
 
 import (
-	"context"
-	"fmt"
-	"io"
 	"time"
 
 	"go.uber.org/zap"
@@ -30,82 +27,4 @@ func NewRepository(cfg *config.Configuration, logger *zap.Logger, s3 s3storage.S
 		Logger:  logger.With(zap.String("repository", "file_storage")),
 		Storage: s3,
 	}
-}
-
-// StoreEncryptedData uploads encrypted file data to S3 and returns the storage path
-func (impl *fileStorageRepositoryImpl) StoreEncryptedData(ownerID string, fileID string, encryptedData []byte) (string, error) {
-	ctx := context.Background()
-
-	// Generate a storage path using a deterministic pattern
-	storagePath := fmt.Sprintf("users/%s/files/%s", ownerID, fileID)
-
-	// Always store encrypted data as private
-	err := impl.Storage.UploadContentWithVisibility(ctx, storagePath, encryptedData, false)
-	if err != nil {
-		impl.Logger.Error("Failed to store encrypted data",
-			zap.String("fileID", fileID),
-			zap.String("ownerID", ownerID),
-			zap.Error(err))
-		return "", err
-	}
-
-	return storagePath, nil
-}
-
-// GetEncryptedData retrieves encrypted file data from S3
-func (impl *fileStorageRepositoryImpl) GetEncryptedData(storagePath string) ([]byte, error) {
-	ctx := context.Background()
-
-	// Get the encrypted data
-	reader, err := impl.Storage.GetBinaryData(ctx, storagePath)
-	if err != nil {
-		impl.Logger.Error("Failed to get encrypted data",
-			zap.String("storagePath", storagePath),
-			zap.Error(err))
-		return nil, err
-	}
-	defer reader.Close()
-
-	// Read all data into memory
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		impl.Logger.Error("Failed to read encrypted data",
-			zap.String("storagePath", storagePath),
-			zap.Error(err))
-		return nil, err
-	}
-
-	return data, nil
-}
-
-// DeleteEncryptedData removes encrypted file data from S3
-func (impl *fileStorageRepositoryImpl) DeleteEncryptedData(storagePath string) error {
-	ctx := context.Background()
-
-	// Delete the encrypted data
-	err := impl.Storage.DeleteByKeys(ctx, []string{storagePath})
-	if err != nil {
-		impl.Logger.Error("Failed to delete encrypted data",
-			zap.String("storagePath", storagePath),
-			zap.Error(err))
-		return err
-	}
-
-	return nil
-}
-
-// GeneratePresignedURL creates a time-limited URL for downloading the file directly
-func (impl *fileStorageRepositoryImpl) GeneratePresignedURL(storagePath string, duration time.Duration) (string, error) {
-	ctx := context.Background()
-
-	// Generate presigned URL
-	url, err := impl.Storage.GetPresignedURL(ctx, storagePath, duration)
-	if err != nil {
-		impl.Logger.Error("Failed to generate presigned URL",
-			zap.String("storagePath", storagePath),
-			zap.Error(err))
-		return "", err
-	}
-
-	return url, nil
 }
